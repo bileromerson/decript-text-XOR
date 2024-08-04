ea46b007be518ee57c8399726176ffbe2e3dfcb5
bileromerson <158579506+bileromerson@users.noreply.github.com>
2024-08-04 14:56:12 -0300

    Add files via upload

---
+++ decript-XOR.py
@@ -0,0 +1,23 @@
+import string
+from pwn import *
+
+charset = string.ascii_letters + string.digits
+enc_flag = bytes.fromhex(input("enter your encripted text:"))
+part_flag = b'THM{'
+
+part_key = xor(enc_flag, part_flag)[:4]
+
+#print(part_key)
+
+for c in charset:
+    key = part_key + c.encode()
+    dec_flag = xor(enc_flag, key).decode()
+    
+    if dec_flag[-1] =='}':
+        print("")
+        print("<===============================================================================================================>")
+        print("")
+        print(f"key:{key.decode()}")
+        print(f"flag:{dec_flag}")
+
+
